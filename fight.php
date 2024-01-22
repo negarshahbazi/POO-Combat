2ce3c09d87c49ea7a5af32c85c01e8526fee3e3b
john Doe <negarshahbazi.official@gmail.com>
2024-01-22 13:31:27 +0100

    zeez

--- fight.php
+++ fight.php
@@ -62,9 +62,9 @@ $heroesManager->update($hero);
 </div>
 <?php } ?>
 <!-- hero and monster -->
-  <div class=" d-flex justify-content-center align-items-center">
-    <div class="mb-5"><img class="image" src="<?php echo $hero->getChoisirHero()?>" alt=""></div>
-    <div class="mb-5"><img class="image" src="./assets/monster.gif" alt=""></div>
+  <div class=" d-flex justify-content-center align-items-center place">
+    <div class="mb-5"><img class="image w-50" src="<?php echo $hero->getChoisirHero()?>" alt=""></div>
+    <div class="mb-5"><img class="image  w-50" src="./assets/monster.gif" alt=""></div>
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
